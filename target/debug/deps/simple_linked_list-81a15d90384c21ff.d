2f4a760866712a5418bab1708e02983de4e53291
mathanaruban <jonny@MacBook-Air-de-mathanaruban.local>
2021-10-22 11:03:11 +0200

    Premier Commit

--- target/debug/deps/simple_linked_list-81a15d90384c21ff.d
+++
@@ -1,5 +0,0 @@
-/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-81a15d90384c21ff.rmeta: src/lib.rs
-
-/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-81a15d90384c21ff.d: src/lib.rs
-
-src/lib.rs:
