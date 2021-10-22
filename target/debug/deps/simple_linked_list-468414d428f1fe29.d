a1bb9eb7c30061707b0b152e5c185b6474f9c2ad
mathanaruban <jonny@MacBook-Air-de-mathanaruban.local>
2021-10-22 11:00:17 +0200

    Premier Commit

---
+++ target/debug/deps/simple_linked_list-468414d428f1fe29.d
@@ -0,0 +1,5 @@
+/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-468414d428f1fe29.rmeta: tests/simple-linked-list.rs
+
+/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-468414d428f1fe29.d: tests/simple-linked-list.rs
+
+tests/simple-linked-list.rs:
---
+++ target/debug/deps/simple_linked_list-d844ab8ddd92f883.d
@@ -0,0 +1,5 @@
+/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-d844ab8ddd92f883.rmeta: src/lib.rs
+
+/home/darnuria/exercism/rust/simple-linked-list/target/debug/deps/simple_linked_list-d844ab8ddd92f883.d: src/lib.rs
+
+src/lib.rs:
